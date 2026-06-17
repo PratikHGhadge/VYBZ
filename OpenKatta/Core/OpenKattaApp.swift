c118f0702a3c64a140343564b07e8e88ca800129
Pratik Ghadge <pratikhg.2001@gmail.com>
2026-06-17 10:19:40 +0530

    [iOS][Firebase] feat: add Firebase SDK framework dependencies and configure app initialization

--- OpenKatta/Core/OpenKattaApp.swift
+++ OpenKatta/Core/OpenKattaApp.swift
@@ -6,17 +6,28 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+	func application(_ application: UIApplication,
+					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		FirebaseApp.configure()
+		return true
+	}
+}
+
 
 @main
 struct OpenKattaApp: App {
+	@StateObject private var viewModel = MainViewModel()
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
-				.onAppear {
-					for family in UIFont.familyNames.sorted() {
-						let names = UIFont.fontNames(forFamilyName: family)
-					}
-				}
+			NavigationStack {
+				MainView(viewModel: viewModel)
+			}
         }
     }
 }
---
+++ OpenKatta/ViewModels/MainViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewModel.swift
+//  OpenKatta
+//
+//  Created by Pratik Haridas Ghadge on 17/06/26.
+//
+
+import Foundation
+import Combine
+
+class MainViewModel: ObservableObject {
+
+	func signOut() {
+		//
+	}
+}
